afb22c4f079699ae76e2e21449d475dafd1a77d4
Silvio-SC <vinhoterranossa@hotmail.com>
2024-01-10 17:50:57 -0300

    feat: Finalizando a parte de atividades

---
+++ src/app/sobre-nos/page.tsx
@@ -0,0 +1,24 @@
+export default function recursos() {
+    return (
+      <main>
+        <section className="
+          flex min-h-full flex-col items-center justify-center px-24 py-48 font-roboto
+          bg-[url('../assets/Background.png')] bg-no-repeat bg-cover bg-center
+          ">
+          <div className="flex flex-col z-10 max-w-5xl w-full gap-12">
+              <h1 className="
+                animate-aparecer text-5xl font-normal text-center drop-shadow-yellow font-['Dancing_Script'] 
+                ">
+                Igreja Batista Nova Aurora
+              </h1>
+              <h2 className='
+                animate-aparecer text-6xl font-normal text-center drop-shadow-yellow
+              '>
+                "Vivendo e compartilhando o Evangelho da cruz."
+              </h2>
+          </div>
+        </section>
+      </main>
+    )
+  }
+  
--- src/components/activities/index.tsx
+++ src/components/activities/index.tsx
@@ -3,25 +3,47 @@ import logo from "../../assets/logo/logo.png"
 
 export const Activities = () => {
     return(
-        <section className="p-8 bg-gray-100 flex flex-col items-center gap-12">
+        <section className="p-8 bg-green-50 flex flex-col items-center gap-12">
             <h2 className="font-['Dancing_Script'] text-5xl font-normal drop-shadow-yellow">
                 Atividades
             </h2>
-            <div className="w-auto">
-                <div className="bg-gray-50 border-black">
-                    <h3 className="text-center font-['Roboto'] p- pb-0 border-b border-black">Domingo</h3>
-                    <ul className="flex flex-col gap-3 p-4">
-                        <li className="border-b border-black">Manhã</li>
-                        <li>
-                            <p>Culto de Oração</p>
-                            <p>07:30</p>
+            <div className="w-auto flex gap-4">
+                <div className="bg-transparent">
+                    <h3 className="text-center font-['Roboto'] pb-0 border-b-2 border-black text-xl font-medium text-xl font-medium">Domingo</h3>
+                    <ul className="flex flex-col gap-2 p-2 text-white">
+                        <li className="bg-emerald-500 rounded py-1 px-3 hover:text-gray-100 hover:bg-emerald-600 duration-300">07:30 - Culto de Oração</li>
+                        <li className="bg-emerald-500 rounded py-1 px-3 hover:text-gray-100 hover:bg-emerald-600 duration-300">08:30 - Escola Biblica Dominical</li>
+                        <li className="bg-emerald-500 rounded py-1 px-3 hover:text-gray-100 hover:bg-emerald-600 duration-300">10:00 - Culto Matutino</li>
+                        <li className="bg-emerald-500 rounded py-1 px-3 hover:text-gray-100 hover:bg-emerald-600 duration-300">18:00 - União de Treinamento</li>
+                        <li className="bg-emerald-500 rounded py-1 px-3 hover:text-gray-100 hover:bg-emerald-600 duration-300">19:15 - Culto Noturno</li>
+                    </ul>
+                </div>
+                <div className="bg-transparent">
+                    <h3 className="text-center font-['Roboto'] pb-0 border-b-2 border-black text-xl font-medium">Quarta</h3>
+                    <ul className="flex flex-col gap-3 p-2 text-white">
+                        <li className="bg-emerald-500 rounded py-1 px-3 hover:text-gray-100 hover:bg-emerald-600 duration-300">18:30 - Culto de Oração</li>
+                        <li className="bg-emerald-500 rounded py-1 px-3 hover:text-gray-100 hover:bg-emerald-600 duration-300">19:30 - Estudo Bíblico</li>
+                    </ul>
+                </div>
+                <div className="bg-transparent">
+                    <h3 className="text-center font-['Roboto'] pb-0 border-b-2 border-black text-xl font-medium">Outros Dias</h3>
+                    <ul className="flex flex-col gap-3 p-2 text-white">
+                        <li className="bg-emerald-500 rounded py-1 px-3 hover:text-gray-100 hover:bg-emerald-600 duration-300">
+                            <p>Segunda</p>
+                            <p>19:00 - Embaixadores e Mensageiras do Rei</p>
+                            <p>19:30 - Ide e Pregai</p>
                         </li>
-                        <li>08:30 - Escola Biblica Dominical</li>
-                        <li>10:00 - Culto Matutino</li>
+                        <li className="bg-emerald-500 rounded py-1 px-3 hover:text-gray-100 hover:bg-emerald-600 duration-300">
+                            <p>Sexta</p>
+                            <p>18:00 - Orquestra</p>
+                        </li>
+                        <li className="bg-emerald-500 rounded py-1 px-3 hover:text-gray-100 hover:bg-emerald-600 duration-300">
+                            <p>Sabado</p>
+                            <p>14:00 - Aulas de Música</p>
+                        </li>
                     </ul>
                 </div>
             </div>
-
         </section>
     )
 }
