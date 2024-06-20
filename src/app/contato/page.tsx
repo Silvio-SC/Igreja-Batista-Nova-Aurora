5ac4198c2b084a781fc9c85985aa0bdc055b1f18
Silvio <vinhoterranossa@hotmail.com>
2024-06-19 23:17:56 -0300

    style: estilizando o input

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,14 +1,20 @@
 'use client'
 
 import ContactForm from '@/components/ContactForm'
-import Image from 'next/image'
+import { Footer } from '@/components/footer'
+
 
 export default function Contato() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <ContactForm />
-      </div>
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <section className="px-8 py-4 bg-green-50 flex flex-col items-center gap-24"> 
+          <h2 className="font-['Dancing_Script'] text-5xl font-normal drop-shadow-yellow xl:text-7xl">
+              Entre em Contato Conosco
+          </h2>
+          <ContactForm />
+        </section>
+      </main>
+    </>
   )
 }
--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -9,6 +9,7 @@ const ContactForm = () => {
     e.preventDefault();
     const form = e.target;
     const data = new FormData(form);
+    console.log(data)
     const response = await fetch("https://formspree.io/f/myzgggbl", {
       method: "POST",
       body: data,
@@ -18,6 +19,7 @@ const ContactForm = () => {
     });
 
     if (response.ok) {
+    // if (true) {
       form.reset();
       setStatus("SUCCESS");
     } else {
@@ -26,21 +28,27 @@ const ContactForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Seu nome:
-        <input type="text" name="name" required />
-      </label>
-      <label>
-        Seu email:
-        <input type="email" name="email" required />
-      </label>
-      <label>
-        Escreva uma mensagem:
-        <textarea name="message" required></textarea>
-      </label>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-6 min-w-full">
+      <div className="flex flex-col">
+        <label htmlFor="name" className="text-xl drop-shadow"> Seu Nome </label>
+        <input type="text" name="name" id="name" required 
+          className="outline-none p-1 bg-transparent border-b-2 border-gray-300 hover:border-gray-700 focus:border-gray-700"
+          placeholder="Coloque seu nome aqui."
+        />
+      </div>
+
+      <label htmlFor="email"> SEU EMAIL </label>
+      <input type="email" name="email" id="email" required />
+
+      <label htmlFor="tel"> SEU CELULAR </label>
+      <input type="tel" name="telefone" id="tel" required />
+
+      <label htmlFor="message"> ESCREVA SUA MENSAGEM </label>
+      <textarea name="message" id="message" required></textarea>
+
       {status === "SUCCESS" ? <p>Enviado com Sucesso!</p> : <button type="submit">Submit</button>}
       {status === "ERROR" && <p>Ooops! Aconteceu algum erro.</p>}
+
     </form>
   );
 };
